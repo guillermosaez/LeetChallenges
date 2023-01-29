25ef5ed41be1b059bd2388ecfc8452c54230f584
Guillermo Sáez <guillermosaezsanchez@gmail.com>
2023-01-29 12:47:50 +0100

    TwoSumV2 implementation with Dictionary

--- 1-TwoSum/TwoSumTests.cs
+++ 1-TwoSum/TwoSumTests.cs
@@ -2,54 +2,72 @@ namespace LeetChallenges._1_TwoSum;
 
 public class TwoSumTests
 {
-	[Theory]
-	[InlineData(null, typeof(ArgumentNullException))]
-	[InlineData(new int[] { }, typeof(ArgumentOutOfRangeException))]
-	[InlineData(new[] { 1 }, typeof(ArgumentOutOfRangeException))]
-	[MemberData(nameof(GetNumbersMaxLength))]
-	public void Numbers_Null(int[] numbers, Type exceptionType)
-	{
-		Action testCode = () => TwoSumValidations.ValidateNumbers(numbers);
-		Exception? exception = Record.Exception(testCode);
-		Assert.IsType(exceptionType, exception);
-	}
+    [Theory]
+    [InlineData(null, typeof(ArgumentNullException))]
+    [InlineData(new int[] { }, typeof(ArgumentOutOfRangeException))]
+    [InlineData(new[] { 1 }, typeof(ArgumentOutOfRangeException))]
+    [MemberData(nameof(GetNumbersMaxLength))]
+    public void Numbers_Null(int[] numbers, Type exceptionType)
+    {
+        Action testCode = () => TwoSumValidations.ValidateNumbers(numbers);
+        Exception? exception = Record.Exception(testCode);
+        Assert.IsType(exceptionType, exception);
+    }
 
-	public static IEnumerable<object[]> GetNumbersMaxLength()
-	{
-		int numbersMaxLength = (int)Math.Pow(10, 4);
-		return new[]
-		{
-			new object[] { Enumerable.Range(1, numbersMaxLength + 1).ToArray(), typeof(ArgumentOutOfRangeException) }
-		};
-	}
+    public static IEnumerable<object[]> GetNumbersMaxLength()
+    {
+        int numbersMaxLength = (int)Math.Pow(10, 4);
+        return new[]
+        {
+            new object[] { Enumerable.Range(1, numbersMaxLength + 1).ToArray(), typeof(ArgumentOutOfRangeException) }
+        };
+    }
 
-	[Theory]
-	[MemberData(nameof(GetTargetLimits))]
-	public void Target_Limits(int target, Type exceptionType)
-	{
-		Action testCode = () => TwoSumValidations.ValidateTarget(target);
-		Exception? exception = Record.Exception(testCode);
-		Assert.IsType(exceptionType, exception);
-	}
+    [Theory]
+    [MemberData(nameof(GetTargetLimits))]
+    public void Target_Limits(int target, Type exceptionType)
+    {
+        Action testCode = () => TwoSumValidations.ValidateTarget(target);
+        Exception? exception = Record.Exception(testCode);
+        Assert.IsType(exceptionType, exception);
+    }
 
-	public static IEnumerable<object[]> GetTargetLimits()
-	{
-		int higherLimit = (int)Math.Pow(10, 9);
-		int lowerLimit = higherLimit * -1;
-		return new[]
-		{
-			new object[] { higherLimit + 1, typeof(ArgumentOutOfRangeException) },
-			new object[] { lowerLimit - 1, typeof(ArgumentOutOfRangeException) }
-		};
-	}
+    public static IEnumerable<object[]> GetTargetLimits()
+    {
+        int higherLimit = (int)Math.Pow(10, 9);
+        int lowerLimit = higherLimit * -1;
+        return new[]
+        {
+            new object[] { higherLimit + 1, typeof(ArgumentOutOfRangeException) },
+            new object[] { lowerLimit - 1, typeof(ArgumentOutOfRangeException) }
+        };
+    }
 
-	[Theory]
-	[InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
-	[InlineData(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
-	[InlineData(new[] { 3, 3 }, 6, new[] { 0, 1 })]
-	public void V1Correct(int[] numbers, int target, int[] expected)
-	{
-		int[] result = TwoSumV1.Execute(numbers, target);
-		Assert.Equal(expected, result);
-	}
+    [Theory]
+    [MemberData(nameof(GetCorrectDataSets))]
+    public void V1Correct(int[] numbers, int target, int[] expected)
+    {
+        int[] result = TwoSumV1.Execute(numbers, target);
+        Assert.NotNull(result);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetCorrectDataSets))]
+    public void V2Correct(int[] numbers, int target, int[] expected)
+    {
+        int[] result = TwoSumV2.Execute(numbers, target);
+        Assert.NotNull(result);
+        Assert.Equal(expected, result);
+    }
+
+    public static IEnumerable<object[]> GetCorrectDataSets()
+    {
+        return new[]
+        {
+            new object[] { new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 } },
+            new object[] { new[] { 3, 2, 4 }, 6, new[] { 1, 2 } },
+            new object[] { new[] { 3, 3 }, 6, new[] { 0, 1 } }
+        };
+    }
 }
