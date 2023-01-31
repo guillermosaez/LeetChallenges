0ef3d021c4704feed7b39b6c907b2810df065157
Guillermo Sáez <guillermosaezsanchez@gmail.com>
2023-01-31 18:07:11 +0100

    Validate + use Stack<int>

--- LeetChallenges/2-AddTwoNumbers/AddTwoNumbers.cs
+++ LeetChallenges/2-AddTwoNumbers/AddTwoNumbers.cs
@@ -3,42 +3,66 @@ namespace LeetChallenges._2_AddTwoNumbers;
 //https://leetcode.com/problems/add-two-numbers/
 public static class AddTwoNumbers
 {
-	public static ListNode Sum(ListNode list1, ListNode list2)
-	{
-		//TODO: Validaciones
-		int list1ReverseSumValue = CalculateReversedSumValue(list1);
-		int list2ReverseSumValue = CalculateReversedSumValue(list2);
-		int sum = list1ReverseSumValue + list2ReverseSumValue;
+    public static ListNode Sum(ListNode list1, ListNode list2)
+    {
+        Validate(list1);
+        Validate(list2);
 
-		List<int> sumValues = new();
-		while (sum >= 10)
-		{
-			sumValues.Add(sum % 10);
-			sum /= 10;
-		}
-		sumValues.Add(sum);
+        int list1ReverseSumValue = CalculateReversedSumValue(list1);
+        int list2ReverseSumValue = CalculateReversedSumValue(list2);
+        int sum = list1ReverseSumValue + list2ReverseSumValue;
 
-		sumValues.Reverse();
-		ListNode resultNode = null;
-		foreach (int value in sumValues)
-		{
-			resultNode = new(value, resultNode);
-		}
-		return resultNode;
-	}
+        Stack<int> sumValues = new();
+        do
+        {
+            sumValues.Push(sum % 10);
+            sum /= 10;
+        } while (sum > 0);
+        
+        ListNode resultNode = null;
+        while (sumValues.TryPop(out int value))
+        {
+            resultNode = new(value, resultNode);
+        }
+        return resultNode;
+    }
 
-	private static int CalculateReversedSumValue(ListNode list)
-	{
-		int result = 0;
-		int multiplier = 1;
-		while (list != null)
-		{
-			result += list.Value * multiplier;
-			multiplier *= 10;
-			list = list.Next;
-		}
-		return result;
-	}
+    private static void Validate(ListNode listNode)
+    {
+        byte numberOfNodes = 1;
+        ValidateValue(listNode);
+        while (listNode.Next != null)
+        {
+            numberOfNodes++;
+            if (numberOfNodes > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(listNode));
+            }
+            listNode = listNode.Next;
+            ValidateValue(listNode);
+        }
+    }
+
+    private static void ValidateValue(ListNode listNode)
+    {
+        if (listNode.Value is < 0 or > 9)
+        {
+            throw new ArgumentOutOfRangeException(nameof(ListNode.Value));
+        }
+    }
+
+    private static int CalculateReversedSumValue(ListNode list)
+    {
+        int result = 0;
+        int multiplier = 1;
+        while (list != null)
+        {
+            result += list.Value * multiplier;
+            multiplier *= 10;
+            list = list.Next;
+        }
+        return result;
+    }
 }
 
 public record ListNode(int Value = 0, ListNode? Next = null);
